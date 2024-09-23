a208e418edaff1333af1447718d43eb60eb511fc
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-23 11:34:33 +0600

    Using Context API to avoid prop drilling (Naive approach)

--- Counter-Game/src/App.jsx
+++ Counter-Game/src/App.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
 import { Card, Typography, Button } from "@mui/material";
+import { createContext, useContext } from "react";
+
+const CountContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0);
+
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Card style={{ padding: 20, width: 500, verticalAlign: "center" }}>
-        <Typography variant="h5">Welcome to the counter game</Typography>
-        <br />
-        <Buttons count={count} setCount={setCount} />
-        <br />
-        <CountComponent count={count} />
-      </Card>
-    </div>
+    <CountContext.Provider value={{
+      count: count,
+      setCount: setCount
+    }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card style={{ padding: 20, width: 500, verticalAlign: "center" }}>
+          <Typography variant="h5">Welcome to the counter game</Typography>
+          <br />
+          <Buttons />
+          <br />
+          <CountComponent />
+        </Card>
+      </div>
+    </CountContext.Provider>
   );
 }
 
-function Buttons({ count, setCount }) {
+function Buttons() {
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Increase count={count} setCount={setCount} />
-      <Decrease count={count} setCount={setCount} />
+      <Increase />
+      <Decrease />
     </div>
   );
 }
 
-function Increase({ count, setCount }) {
+function Increase() {
+  const {count, setCount} = useContext(CountContext);
   return (
     <div>
       <Button variant={"contained"} onClick={() => setCount(count + 1)}>
@@ -35,7 +45,8 @@ function Increase({ count, setCount }) {
   );
 }
 
-function Decrease({ count, setCount }) {
+function Decrease() {
+  const {count, setCount} = useContext(CountContext);
   return (
     <div>
       <Button variant={"contained"} onClick={() => setCount(count - 1)}>
@@ -45,7 +56,8 @@ function Decrease({ count, setCount }) {
   );
 }
 
-function CountComponent({ count }) {
+function CountComponent() {
+  const {count} = useContext(CountContext);
   return (
     <div>
       <Typography variant="h6" textAlign={"center"}>
